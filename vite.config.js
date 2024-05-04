41c40b09c4711e0776b9c895a29235251c3aa930
Pablo Cortés <cortesoltra@gmail.com>
2024-05-04 12:11:09 +0200

    Update vite.config for local

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 export default defineConfig({
     server: {
         hmr: {
-            host: "0.0.0.0",
+            host: "localhost",
         },
         port: 3000,
         host: true,
